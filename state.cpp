8cb6cb416e7a28c4c66e007b001592dec6e6878b
Sam Royston <sfoxroyston@gmail.com>
2016-12-20 23:50:43 -0500

    state

--- state.cpp
+++ state.cpp
@@ -8,21 +8,31 @@
 using namespace std::chrono;
 
 std::vector<std::pair<char *, int> > State::match_mdns_aruco(std::vector<char *> hosts, std::vector<int> ids) {
-
+    //TODO: finish
+    std::vector<std::pair<char *, int> > op;
+    return op;
 }
 
 std::vector<Bot> State::bots_for_ids(std::vector<char *> hosts, std::vector<int> ids) {
+    //TODO: finish
+    std::vector<Bot> ds;
+    return ds;
 }
 
 
 void State::update(cv::Mat image) {
     State::difference_image = State::current_image - image;
-    State::image_queue.push(image);
+
     State::current_image = image;
-    milliseconds ms = duration_cast< milliseconds >(
-            system_clock::now().time_since_epoch()
-    );
+    if(State::image_queue.size() >= State::hist_length){
+    	State::image_queue.pop_back();
+    }
+
+    unsigned long ms = duration_cast< milliseconds >
+	    (system_clock::now().time_since_epoch()).count();
+    t_frame current;
+    current.image = image;
+    current.ms = ms;
+    State::image_queue.push_front(current);
 }
 
-
-cv::Mat current_image = cv::Mat::zeros(10, 10, CV_32F);
--- state.h
+++ state.h
@@ -3,7 +3,7 @@
 //
 #include "bot.h"
 #include <opencv2/core.hpp>
-#include <queue>
+#include <deque>
 #include <vector>
 #include <stdlib.h>
 
@@ -15,22 +15,26 @@
  * there is only meant to be one state per process
  */
 
+struct t_frame{
+	cv::Mat image;
+	unsigned long ms;
+};
 
 class State{
 public:
     std::vector<std::pair<char *, int> > match_mdns_aruco(std::vector<char *> hosts, std::vector<int> ids);
     std::vector<Bot> bots_for_ids(std::vector<char *> hosts, std::vector<int> ids);
     void update(cv::Mat image);
-
+    static int hist_length;
     static std::vector<Bot> devices;
     static cv::Mat current_image;
     static cv::Mat difference_image;
-    static std::queue<cv::Mat> image_queue;
+    static std::deque<t_frame> image_queue;
 };
 
-
+int State::hist_length = 40;
 std::vector<Bot> State::devices;
-std::queue<cv::Mat> State::image_queue;
+std::deque<t_frame>State::image_queue;
 cv::Mat State::current_image;
 cv::Mat State::difference_image;
 
