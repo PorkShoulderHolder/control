290c78691a8e93de22a944e9f355f22ba624f762
Sam Royston <sfoxroyston@gmail.com>
2017-01-16 17:19:42 -0500

    finished update rule q learning

--- src/agent.cpp
+++ src/agent.cpp
@@ -73,6 +73,33 @@ std::vector<StateAction> StateActionSpace::get_action_values(int state) {
     return this->at((unsigned long)state);
 }
 
+std::vector<StateAction> StateActionSpace::get_action_values(StateAction s) {
+    return this->get_action_values(s.x, s.y);
+}
+
+bool state_action_comparator(const StateAction& l, const StateAction& r){
+    return l.value < r.value;
+};
+
+StateAction StateActionSpace::get_greedy_action(StateAction s) {
+    std::vector<StateAction> sa = this->get_action_values(s.x, s.y);
+    StateAction max_el = *std::max_element(sa.begin(), sa.end(), state_action_comparator);
+    return max_el;
+}
+
+
+float StateActionSpace::get_action_value(StateAction s) {
+    return this->get_action_value(s.x, s.y, s.action);
+}
+
+StateAction StateActionSpace::get_state_action(StateAction s){
+    /*
+     * Fills in the Q-value(s) of a StateAction with only action and state components
+     */
+    std::vector<StateAction> actions = this->get_action_values(s.x, s.y);
+    return actions[s.action];
+}
+
 float StateActionSpace::get_action_value(int state_x, int state_y, int action) {
     std::vector<StateAction> actions = this->get_action_values(state_x, state_y);
     return actions[action].value;
@@ -86,21 +113,42 @@ void StateActionSpace::set_action_value(int state_x, int state_y, int action, fl
     this->set_action_value(this->count_x * state_y + state_x, action, value);
 }
 
+void StateActionSpace::set_action_value(StateAction s, float value) {
+    this->set_action_value(s.x, s.y, s.action, value);
+}
+
 /*
  *
  *  ========================================================================================
  *
  */
 
-Agent::Agent(const StateActionSpace &O) : Q(O) {
+Agent::Agent(const StateActionSpace &O, std::function reward) : Q(O) {
     int resolution = 100;
     int actions = 4;
     StateActionSpace *sa = new StateActionSpace(resolution, resolution, actions);
     this->Q = *sa;
+    this->reward = reward;
 }
 
 Agent::Agent(const StateActionSpace &O, std::string fn) : Q(O) {
     StateActionSpace *sa = new StateActionSpace(fn);
     this->Q = *sa;
+}
 
+
+
+void Agent::update(StateAction s0, StateAction s1) {
+    /*
+     * This function performs the crucial update step of the agent's Q-matrix;
+     *
+     * s0: state prior to taking action 'a' (only contains valid x, y, action fields)
+     * s1: state after taking action specified in s0 (only contains valid x, y fields)
+     *
+     */
+    s0 = this->Q.get_state_action(s0);
+    StateAction greedy_option = this->Q.get_greedy_action(s1);
+    float gradient = this->reward(s1) + this->discount * greedy_option.value - s0.value;
+    float q0 = s0.value + this->learning_rate * (gradient);
+    this->Q.set_action_value(s0, q0);
 }
--- src/agent.h
+++ src/agent.h
@@ -14,6 +14,9 @@
 #define CONTROL_AGENT_H
 
 struct StateAction{
+    int y;
+    int x;
+    int action;
     float value;
     int visits;
 };
@@ -24,25 +27,37 @@ public:
     StateActionSpace(std::string fn);
     std::vector<StateAction> get_action_values(int state);
     std::vector<StateAction> get_action_values(int state_x, int state_y);
+    std::vector<StateAction> get_action_values(StateAction s);
+    StateAction get_greedy_action(StateAction s);
+    StateAction get_state_action(StateAction s);
     float get_action_value(int state_x, int state_y, int action);
+    float get_action_value(StateAction s);
+
     void set_action_value(int state_x, int state_y, int action, float value);
     void set_action_value(int state, int action, float value);
+    void set_action_value(StateAction s, float value);
     void save(std::string fn);
 
     int state_count;
     int count_x;
     int count_y;
     int action_count;
-
 private:
     void init(int states, int actions);
 };
 
+
+
 class Agent {
 public:
-    Agent::Agent(const StateActionSpace &O);
+    Agent(const StateActionSpace &O, std::function reward);
     Agent(const StateActionSpace &O, std::string fn);
     void serialize(std::string fn);
+    void update(StateAction s0, StateAction s1);
+
+    float learning_rate;
+    float discount;
+    std::function<float(StateAction)> reward;
 
 private:
     StateActionSpace Q;
